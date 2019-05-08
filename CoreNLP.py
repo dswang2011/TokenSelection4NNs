cafba4e7eb9487142e370b15f907442f34ed1164
Dongsheng Wang <dongsheng@angua>
2019-05-08 15:36:09 +0200

    token selection

--- CoreNLP.py
+++ CoreNLP.py
@@ -1,14 +1,16 @@
 
 from stanfordcorenlp import StanfordCoreNLP
 import numpy as np
+import nltk
+from nltk.corpus import stopwords
 
 nlp = StanfordCoreNLP(r'/home/dongsheng/data/resources/stanford-corenlp-full-2018-10-05')
 
 # sentence = 'Guangdong University of Foreign Studies is located in Guangzhou.'
-sentence = 'My name is Dongsheng, which is named by my father.'
+sentence = 'This is a sample sentence, showing off the stop words filtration.'
 # print('Tokenize:', nlp.word_tokenize(sentence))
 # print('Part of Speech:', nlp.pos_tag(sentence))
-# print('Named Entities:', nlp.ner(sentence))
+print('Named Entities:', nlp.ner(sentence))
 # print('Constituency Parsing:', nlp.parse(sentence))
 # print('Dependency Parsing:', nlp.dependency_parse(sentence))
 
@@ -16,9 +18,10 @@ sentence = 'My name is Dongsheng, which is named by my father.'
 
 
 # punctuation tags: . :
-noun_list = ['NN','NNP','NNPS']
-verb_list = ['VB', 'VBZ', 'VBD', 'VBN']
-adjective_list = ['JJ']
+noun_list = ['NN','NNS','NNP','NNPS']
+# VB, VBZ, VBD, VBG, VBN, VBP
+verb_list = ['VB', 'VBZ', 'VBD', 'VBG','VBN','VBP']
+adjective_list = ['JJ','JJR','JJS']
 
 def get_tokens_POS(sentence,tag_list):
 	token_tag_list = nlp.pos_tag(sentence)
@@ -27,6 +30,26 @@ def get_tokens_POS(sentence,tag_list):
 		if v in tag_list:
 			res_tokens.append(k)
 	return res_tokens
+# c(3,1)+c(3,2)+c(3,3) = 7 combinations
+def text2token_POS(text_list,label_list):
+	Noun=[]
+	Verb=[]
+	Adjective=[]
+	Noun_Verb=[]
+	Noun_Adjective=[]
+	Verb_Adjective=[]
+	Noun_Verb_Adjective=[]
+
+	for text in text_list:
+		Noun.append(get_tokens_POS(text,noun_list))
+		Verb.append(get_tokens_POS(text,verb_list))
+		Adjective.append(get_tokens_POS(text,adjective_list))
+		Noun_Verb.append(get_tokens_POS(text,noun_list+verb_list))
+		Noun_Adjective.append(get_tokens_POS(text,noun_list+adjective_list))
+		Verb_Adjective.append(get_tokens_POS(text,verb_list+adjective_list))
+		Noun_Verb_Adjective.append(get_tokens_POS(text,noun_list+verb_list+adjective_list))
+	return [Noun,Verb,Adjective,Noun_Verb,Noun_Adjective,Verb_Adjective,Noun_Verb_Adjective,label_list]
+
 
 # tree Node
 class Node(object):
@@ -47,7 +70,6 @@ class Node(object):
 			print('(',v.data,[node.data for node in v.children],')')
 			for child in v.children:
 				queue.append(child)
-
 # get depth
 def get_depth(node):
 	if len(node.children)==0:
@@ -68,6 +90,23 @@ def random_select(sentence,select_ratio=1.0):
 	selected_count = int(len(word_list)*select_ratio)
 	selected_index = np.random.choice(len(word_list),selected_count,replace=False)
 	return np.array(word_list)[selected_index].tolist()
+def text2tokens_random(text_list,label_list,select_ratio):
+	sequence = []
+	for text in text_list:
+		sequence.append(random_select(text,select_ratio))
+	return [sequence,label_list]
+
+
+def stopword_removed(sentence):
+	stop_words = set(stopwords.words('english'))
+	word_list = nlp.word_tokenize(sentence)
+	filtered_tokens = [w for w in word_list if not w in stop_words]
+	return filtered_tokens
+def text2tokens_stopword(text_list,label_list):
+	sequence = []
+	for text in text_list:
+		sequence.append(stopword_removed(text))
+	return [sequence,label_list]
 
 
 # cut mean cutting lower layers, 0 means no cutting, 1 means cut one leaf layer, etc.
@@ -76,7 +115,6 @@ def get_token_dependency(sentence,cut=0):
 	# print(tuple_list)
 	word_list = nlp.word_tokenize(sentence)
 	# print(word_list)
-
 	index2Node = {}
 	# buld the tree
 	for tag,point,index in tuple_list:
@@ -117,11 +155,18 @@ def get_token_dependency(sentence,cut=0):
 	print([word_list[node.data-1] for node in sorted_nodes])
 	return [word_list[node.data-1] for node in sorted_nodes]
 
+def text2tokens_dependency(text_list,label_list,cut=1):
+	sequence = []
+	for text in text_list:
+		sequence.append(get_token_dependency(text,cut))
+	return [sequence,label_list]
+
 ###### test #####
 # dependency tree cutting
-res_toknes = get_token_dependency(sentence,4)
+# res_toknes = get_token_dependency(sentence,4)
 # random selection
-print(random_select(sentence,select_ratio=0.2))
-
+# print(random_select(sentence,select_ratio=0.2))
+# stop word removed
+# print(stopword_removed(sentence))
 
 nlp.close() # Do not forget to close! The backend server will consume a lot memery.
